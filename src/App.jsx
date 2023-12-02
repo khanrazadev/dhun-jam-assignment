edb6ca7d0f1f3e5a32a92900c0228500616f738d
raza <razakhanha05@gmail.com>
2023-12-02 21:20:04 +0530

    Added toaster and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" exact Component={Login} />
+      <Route path="/dashboard" exact Component={AdminDashboard} />
     </Routes>
   );
 }
